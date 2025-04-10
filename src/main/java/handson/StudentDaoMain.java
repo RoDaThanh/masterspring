b629bec0d3bf6639729a3e465ec51141dfb9aded
thanh.ro <thanhroda1@gmail.com>
2025-04-10 21:22:28 +0700

    feat: play with FetchType.LAZY

--- src/main/java/handson/StudentDaoMain.java
+++ src/main/java/handson/StudentDaoMain.java
@@ -2,15 +2,22 @@ package handson;
 
 import handson.config.BeanConfig;
 import handson.dao.StudentDaoJpaImpl;
+import handson.model.Book;
 import handson.model.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 public class StudentDaoMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
         StudentDaoJpaImpl studentDao = applicationContext.getBean(StudentDaoJpaImpl.class);
         Student student = new Student("thanh dep trai");
+        student.getBooks().add(new Book("New Book"));
         studentDao.saveStudent(student);
+
+        List<Student> students = studentDao.getStudents();
+        students.forEach(System.out::println);
     }
 }
--- src/main/java/handson/dao/StudentDaoJpaImpl.java
+++ src/main/java/handson/dao/StudentDaoJpaImpl.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceUnit;
 
+import java.util.List;
+
 public class StudentDaoJpaImpl {
 
     @PersistenceUnit
@@ -16,5 +18,13 @@ public class StudentDaoJpaImpl {
         entityManager.persist(student);
         entityManager.getTransaction().commit();
         entityManager.close();
+    }
+
+    public List<Student> getStudents() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<Student> students = entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+        students.forEach(s -> s.getBooks().forEach(System.out::println));
+        entityManager.close();
+        return students;
     }
 }
